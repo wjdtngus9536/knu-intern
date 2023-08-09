3131f371603e8ad1320da40d2d81f9ecaad2fa1c
wjdtngus9536 <wjdtngus9536@gmail.com>
2023-08-09 17:39:12 +0900

    update wrong calculator.py - ver2.0

--- 07_gitScenario/calculator.py
+++ 07_gitScenario/calculator.py
@@ -1,10 +1,15 @@
-def plus(num1, num2):
+def add(num1, num2):
     return num1 + num2
+
+def sub(num1, num2):
+    return num1 * num2
 
 if __name__ == '__main__':
     num1 = 3
     num2 = 7
 
-    a = plus(num1, num2)
+    a = add(num1, num2)
+    b = sub(num1, num2)
 
     print(a)
+    print(b)
