88acf73a40ca23854f85684f2b5bfd73f6bc29b6
wjdtngus9536 <wjdtngus9536@gmail.com>
2023-08-09 17:49:10 +0900

    ver 3.0 - mul

--- 07_gitScenario/calculator.py
+++ 07_gitScenario/calculator.py
@@ -4,12 +4,17 @@ def add(num1, num2):
 def sub(num1, num2):
     return num1 * num2
 
+def mul(num1, num2):
+    return num1 * num2
+
 if __name__ == '__main__':
     num1 = 3
     num2 = 7
 
     a = add(num1, num2)
     b = sub(num1, num2)
+    c = mul(num1, num2)
 
     print(a)
     print(b)
+    print(c)
