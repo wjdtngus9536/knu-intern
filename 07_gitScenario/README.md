3fb345129a6fa195441d4f2fe3014d2942ab759b
wjdtngus9536 <wjdtngus9536@gmail.com>
2023-08-09 18:12:23 +0900

    fix calculator.py ver 2.0

---
+++ 07_gitScenario/README.md
@@ -0,0 +1,6 @@
+## git reset
+usage :
+```git
+git reset 커밋 해시코드
+```
+그냥 reset을 이용한 경우, reset으로 돌아온 커밋 이후의 변경사항은 모두 unstaged 영역에 남는다.
--- 07_gitScenario/calculator.py
+++ 07_gitScenario/calculator.py
@@ -2,7 +2,10 @@ def add(num1, num2):
     return num1 + num2
 
 def sub(num1, num2):
-    return num1 * num2
+    return num1 - num2
+
+def div(num1, num2):
+    return num1 / num2
 
 if __name__ == '__main__':
     num1 = 3
@@ -10,6 +13,8 @@ if __name__ == '__main__':
 
     a = add(num1, num2)
     b = sub(num1, num2)
+    d = div(num1, num2)
 
     print(a)
     print(b)
+    print(d)
